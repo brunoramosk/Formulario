b44a89efe7ddf85e6866ee3087d915d6fc03948c
Bruno Ramos <bkrug@outlook.com.br>
2022-02-08 23:01:11 -0300

    Atualizada

--- javascript.js
+++ javascript.js
@@ -1,37 +1,41 @@
-aUsuarios = [];
-totalUsuarios = 0;
-
-
-function criarUsuario() 
-       
-{
-let usuario = document.getElementById("usuario").value;
-let u = {nome:usuario};
-
-var destino = document.getElementById("lista");
-document.getElementById("selecione").innerHTML += "<option value='" + totalUsuarios + "'>" + usuario ;
-destino.innerHTML += "<option value='" + totalUsuarios + "'>" + usuario + "</option>";
-totalUsuarios++;
+(aUsuarios = []), (totalUsuarios = 0);
+function criarUsuario() {
+  let a = document.getElementById("usuario").value;
+  var b = document.getElementById("lista");
+  (document.getElementById("selecione").innerHTML +=
+    "<option value='" + totalUsuarios + "'>" + a),
+    (b.innerHTML += "<option value='" + totalUsuarios + "'>" + a + "</option>"),
+    totalUsuarios++;
 }
-
-function envia()
-{
-	let usuario = document.getElementById("usuario").value;
-	let telefone = document.getElementById("telefone").value;
-    let dataSemFormatação = document.getElementById("data").value;
-    let nota = document.getElementById("nota").value;
-    let cidade = document.getElementById("cidade").value;
-    let email = document.getElementById("email").value;
-    let sexo = document.getElementById("sexo").value;
-    var destino = document.getElementById("saida");
-
-    var dataFormatada = dataSemFormatação.split("-");
-
-    destino.innerHTML += "<br>" + "Aluno:" + usuario + "<br>" + "Telefone:" + telefone + "<br>" + "Data de Nascimento:" + dataFormatada[2]+ "/" + dataFormatada[1] + "/"+ dataFormatada[0] + "<br>" + "Nota Final:" + nota + "<br>" + "Cidade:" + cidade + "<br>" + "Sexo:" + sexo + "<br>" + "Email:" + email + "<br>";
-    
-   
-    alert ("Dados enviados ! ")
-    
-
+function envia() {
+  let a = document.getElementById("usuario").value,
+    b = document.getElementById("telefone").value,
+    c = document.getElementById("data").value,
+    d = document.getElementById("nota").value,
+    e = document.getElementById("cidade").value,
+    f = document.getElementById("email").value,
+    g = document.getElementById("sexo").value;
+  var h = document.getElementById("saida"),
+    i = c.split("-");
+  (h.innerHTML +=
+    "<br>Aluno:" +
+    a +
+    "<br>Telefone:" +
+    b +
+    "<br>Data de Nascimento:" +
+    i[2] +
+    "/" +
+    i[1] +
+    "/" +
+    i[0] +
+    "<br>Nota Final:" +
+    d +
+    "<br>Cidade:" +
+    e +
+    "<br>Sexo:" +
+    g +
+    "<br>Email:" +
+    f +
+    "<br>"),
+    alert("Dados enviados ! ");
 }
-
